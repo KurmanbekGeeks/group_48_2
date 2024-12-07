f867ebd38bd37b4cd83567f41f3214310172dbdc
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2024-12-07 15:46:52 +0600

    Lesson 2

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
 
+Admins = [995712956, ]
+
 token = config("TOKEN")
 
 bot = Bot(token=token)
---
+++ handlers/echo.py
@@ -0,0 +1,9 @@
+# echo.py
+from aiogram import types, Dispatcher
+
+async def echo_handler(message: types.Message):
+    await message.answer(message.text)
+
+
+def register_echo_handlers(dp: Dispatcher):
+    dp.register_message_handler(echo_handler)
